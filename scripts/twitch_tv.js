90235afb60735e85b9fb77ad0eb50b24a67d2693
Jim Doyle <indellibleink@gmail.com>
2018-05-31 23:15:04 -0400

    got avatars and name with links working

--- scripts/twitch_tv.js
+++ scripts/twitch_tv.js
@@ -15,16 +15,53 @@
 //      console.log(data);
 //    }
 //   });
-
-
-
+var usr=[ "ninja","BlodgetTV", "cretetion", "storbeck", "OgamingSC2"];
+for (i in usr){
+	// Users 
    $.ajax({
     type: 'GET',
-    url: 'https://api.twitch.tv/kraken/streams/ninja',
+    url: 'https://api.twitch.tv/kraken/channels/'+usr[i],
     headers: {
       'Client-ID': 'ykbtvj4v513rrmj2ylqrd03pan0g8z'
     },
-    success: function(data) {
-      console.log(data);
+    success: function(users) {
+    console.log(users);
+    var eLi= document.createElement("li");
+    var uName="<a href="+users.display_name+"></a>";
+    var aImg="<img id='logo' src="+users.logo+">";
+    var aTag="<a href="+users.url+">"+users.display_name+"</a>";
+    eLi.innerHTML=(aImg+" "+aTag);
+    document.getElementById("uList").appendChild(eLi); 
+
     }
    });
+   
+   // streams
+  // $.ajax({
+  //  type: 'GET',
+  //  url: 'https://api.twitch.tv/kraken/streams/'+usr[i],
+  //  headers: {
+  //    'Client-ID': 'ykbtvj4v513rrmj2ylqrd03pan0g8z'
+  //  },
+  //  success: function(strm) {
+  //    console.log(strm);
+  //    if (strm.user == null) {
+//
+  //    }
+  //  }
+  // });
+   
+   // channels
+ //  $.ajax({
+ //   type: 'GET',
+ //   url: 'https://api.twitch.tv/kraken/users/'+usr[i],
+ //   headers: {
+ //     'Client-ID': 'ykbtvj4v513rrmj2ylqrd03pan0g8z'
+ //   },
+ //   success: function(user) {
+ //     console.log();
+ //     var p=usr.pop();
+ //     $("#"+p).append("<img src="+user.logo+">");
+ //   }
+ //  });
+ };
